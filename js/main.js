e0339b6c0b28a5043c5b11f47af47971047d7bcb
Patel <j_patel73637@tss-hr420-dm37.fc.ca>
2019-01-17 16:38:08 -0500

    working files

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 (() => {
     const UserComponent = {
         props: ['name' ,'role'],
-        template: "#usertemplate"
-    }
+        template: "#usertemplate",
+        created: function(){
+            console.log("this is created");
+        },
+
+        methods:{
+            logFromChild(){
+                console.log("logged from component");
+            }, 
+            passEvent(){
+                this.$emit('shutup');
+            }
+        }
+    };
 
     const vn = new Vue({
         el: "#app",
@@ -10,7 +22,12 @@
             testmessage: "sup"
         },
         methods:{
-
+            calledOnParent(){
+                console.log("this method is from vue");
+            }
+        },
+        created: function(){
+            console.log("this is vue created");
         },
         components:{
             'activeusers' : UserComponent
